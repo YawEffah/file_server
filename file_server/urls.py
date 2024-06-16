ec21c9a0818e5f980a8f5e64b29059de6cc83691
YawEffah <effahofficial99@gmail.com>
2024-06-16 18:43:28 +0100

    User-dashboard template updated to handle file share error

--- file_server/urls.py
+++ file_server/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('filebox', include('filebox.urls')),
+    path('filebox/', include('filebox.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
