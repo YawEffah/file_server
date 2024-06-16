d640c642eb48306f9edbfa06d2e9a50e857d5164
YawEffah <effahofficial99@gmail.com>
2024-06-16 15:47:36 +0100

    Register custom user model in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'gender')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'gender'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'gender', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name', 'gender')
+    ordering = ('email',)
+
 
 # Register your models here.
+
+admin.site.register(User, CustomUserAdmin)
+
+
