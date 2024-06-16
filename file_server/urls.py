4376558c1326dceddffc73c2a0c6df8a669ef191
YawEffah <effahofficial99@gmail.com>
2024-06-16 14:45:50 +0100

    Add filebox app and register in settings and urls. This app will handle the main functionalities of the File Server.

--- file_server/urls.py
+++ file_server/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    
+    path('filebox', include('filebox.urls')),
 ]
