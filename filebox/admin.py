8968cee8f3097b9a9668f09668fac10bb5aee5ee
YawEffah <effahofficial99@gmail.com>
2024-06-17 12:35:42 +0100

    Register Email and Dowload Models on the admin page

--- filebox/admin.py
+++ filebox/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import UploadedFile
+from .models import *
 
 admin.site.register(UploadedFile)
+admin.site.register(Email)
+admin.site.register(Download)
--- filebox/urls.py
+++ filebox/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('user_dashboard/', views.user_dashboard, name='user_dashboard'),
     path('upload/', views.upload_view, name='upload'),
     path('update/<int:file_id>', views.update_view, name='update'),
-    path('delete/<int:file_id>', views.delelte_view, name='delete'),
+    path('delete/<int:file_id>', views.delete_view, name='delete'),
     path('download/<int:file_id>', views.download_file, name='download_file'),
     path('share/<int:file_id>', views.share_file, name='share_file')
 ]
